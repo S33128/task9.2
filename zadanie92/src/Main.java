15f113325f8a5a92ec51420c300980a739956c57
s33128 <s33128@pjwstk.edu.pl>
2025-06-17 16:50:20 +0200

    koniec

--- zadanie92/src/Main.java
+++ zadanie92/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item(1, "Mlotek"));
+        items.add(new Item(2, "Wiertarka"));
+        items.add(new Item(3, "Wkretarka"));
+        items.add(new Item(4, "Srubokret"));
+        items.add(new Item(5, "Lopata"));
+
+        for (int i=0; i<items.size(); i++) {
+            items.get(i).show();
+        }
+        HashMap<Integer, String> map = new HashMap<>();
+        for (Item item : items) {
+            map.put(item.id, item.name);
+        }
+        for (Map.Entry<Integer, String> entry : map.entrySet()){
+            System.out.println("ID: "+entry.getKey()+ ", Name: "+entry.getValue());
+        }
     }
 }
